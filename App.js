0957393f88a1b63b91297d9a53bc151f681f8cec
Isaac Miller <isaacbmiller@gmail.com>
2021-01-14 01:52:00 -0600

    Login

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
 import 'react-native-gesture-handler';
-import React, { useState,} from 'react';
+import React, { useState, useEffect} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ScheduleScreen from './screens/ScheduleScreen';
-import CourseDetailScreen from './screens/CourseDetailScreen'
+import CourseDetailScreen from './screens/CourseDetailScreen';
 import UserContext from './utils/UserContext';
-import CourseEditScreen from './screens/CourseEditScreen'
+import CourseEditScreen from './screens/CourseEditScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import {firebase} from './utils/firebase';
+import { Button } from 'react-native';
+
 const Stack = createStackNavigator();
 
 const App = () => {
-  const [user, setUser] = useState({ role: 'admin'});
+  const [user, setUser] = useState(null);
+  const [auth, setAuth] = useState();
+ 
+  useEffect(() => {
+    if (auth && auth.uid) {
+      const db = firebase.database().ref('users').child(auth.uid);
+      const handleData = snap => {
+        setUser({uid: auth.uid, ...snap.val()});
+      }
+      db.on('value', handleData, error => alert(error));
+      return () => { db.off('value', handleData); };
+    } else {
+      setUser(null);
+    }
+  }, [auth]);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((auth) => {
+      setAuth(auth);
+    });
+  }, []);
+
   return (
     
     <UserContext.Provider value={user}>
@@ -17,7 +42,13 @@ const App = () => {
         <Stack.Navigator> 
           <Stack.Screen name="ScheduleScreen"
             component={ScheduleScreen}
-            options={{ title: 'Schedule'}} 
+            options={({navigation}) => ({ 
+            title: "Schedule",
+            headerRight: () => (
+              <SignInButton navigation={navigation} user={user} />
+            ),
+    })
+  } 
           />
           <Stack.Screen name="CourseDetailScreen"
             component={CourseDetailScreen}
@@ -27,10 +58,21 @@ const App = () => {
             component={CourseEditScreen}
             options={{ title: 'Course Editor'}} 
           />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </UserContext.Provider>
   );
 };
+
+const SignInButton = ({ navigation, user }) => (
+  user && user.uid
+  ? <Button title="Logout" color="#448aff"
+      onPress={() => firebase.auth().signOut()}
+    />
+  : <Button title="Sign In" color="#448aff"  
+      onPress={() => navigation.navigate('RegisterScreen')}
+    />
+);
 
 export default App;
